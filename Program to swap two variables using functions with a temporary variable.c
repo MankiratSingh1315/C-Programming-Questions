1f55fd2ddfa8a9aed1900bb2f5625096e10b1bb0
MankiratSingh1315 <msingh2_be22@thapar.edu>
2022-12-29 12:39:18 +0530

    Swap using temp completed

--- Program to swap two variables using functions with a temporary variable.c
+++ Program to swap two variables using functions with a temporary variable.c
@@ -1,1 +1,14 @@
 #include<stdio.h>
+void swap(int *a, int *b){
+	int temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
+int main(){
+	int x,y;
+	printf("Enter 2 numbers:");
+	scanf("%d %d",&x,&y);
+	swap(&x,&y);
+	printf("After swapping the no.'s are %d,%d",x,y);
+}
